9c3b18e4efc102679b58bc360bf633a073569eb1
zzzzdong <jieun8496@icloud.com>
2025-06-26 19:47:44 +0900

    feat: 회원 CRUD

---
+++ src/main/java/com/example/basic_project/task/domain/entity/Task.java
@@ -0,0 +1,25 @@
+package com.example.basic_project.task.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "tasks")
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+}
